209ba99686f5dc101d5738cdf126334dd308c94d
Christoph Gassner <christoph@braurup.at>
2022-07-17 11:24:02 +0200

    Initial Commit

---
+++ FDS_Gassner_Ue10/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FDS_Gassner_Ue10
+//
+//  Created by Christoph Gassner on 17.07.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
